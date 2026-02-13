179915640df5d8f4ac2075eaa1ddf8c22212c57a
Арсений Барзанкаев <a.barzankaev@example.com>
2026-02-14 02:04:27 +0300

    testing: clean project

--- app/middleware/auth.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  // Проверка авторизации
-  const token = useCookie('auth-token')
-  
-  if (!token.value) {
-    return navigateTo('/login')
-  }
-})
-
--- app/middleware/guest.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  // Middleware для страниц, доступных только для неавторизованных пользователей
-  const token = useCookie('auth-token')
-  
-  if (token.value) {
-    return navigateTo('/')
-  }
-})
-
